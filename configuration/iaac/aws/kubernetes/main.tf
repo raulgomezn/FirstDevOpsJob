d45c8a539ce1a7c52bcc00d2a072684f1f1e4607
gomezraul <raul.gomez@grupojurad.com>
2021-06-20 00:09:40 -0500

    ajuste

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -29,9 +29,9 @@ provider "kubernetes" {
  // version                = "~> 1.9"
 }
 
-module "aforo255-cluster3" {
+module "aforo255-cluster4" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "aforo255-cluster3"
+  cluster_name    = "aforo255-cluster4"
   cluster_version = "1.17"
   subnets         = ["subnet-bb3970e4", "subnet-0ede9368"]  #CHANGE # Donot choose subnet from us-east-1e
   #subnets = data.aws_subnet_ids.subnets.ids
@@ -49,11 +49,11 @@ module "aforo255-cluster3" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.aforo255-cluster3.cluster_id
+  name = module.aforo255-cluster4.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.aforo255-cluster3.cluster_id
+  name = module.aforo255-cluster4.cluster_id
 }
 
 
